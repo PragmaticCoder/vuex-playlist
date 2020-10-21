9f55f88cccaa2cd1d5af3ac0a46d7c2db8a04182
Alvi Akbar <alvi.akbar@outlook.com>
2020-10-21 15:46:17 -0600

    Minor code refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,51 @@
 <template>
   <div id="app">
-      <product-list-one v-bind:products="products"></product-list-one>
-      <product-list-two v-bind:products="products"></product-list-two>
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
   </div>
 </template>
 
 <script>
-import ProductListOne from './components/ProductListOne.vue'
-import ProductListTwo from './components/ProductListTwo.vue'
+import ProductListOne from "./components/ProductListOne.vue";
+import ProductListTwo from "./components/ProductListTwo.vue";
 
 export default {
-    name: 'app',
-    components: {
-        'product-list-one': ProductListOne,
-        'product-list-two': ProductListTwo
-    },
-    data () {
-        return {
-            products: [
-                {name: 'Banana Skin', price: 20},
-                {name: 'Shiny Star', price: 40},
-                {name: 'Green Shells', price: 60},
-                {name: 'Red Shells', price: 80}
-            ]
-        }
-    }
-}
+  name: "app",
+  components: {
+    "product-list-one": ProductListOne,
+    "product-list-two": ProductListTwo,
+  },
+  data() {
+    return {
+      products: [{
+          id: 1,
+          name: "Banana Skin",
+          price: 20
+        },
+        {
+          id: 2,
+          name: "Shiny Star",
+          price: 40
+        },
+        {
+          id: 3,
+          name: "Green Shells",
+          price: 60
+        },
+        {
+          id: 4,
+          name: "Red Shells",
+          price: 80
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-body{
-    font-family: Ubuntu;
-    color: #555;
+body {
+  font-family: Ubuntu;
+  color: #555;
 }
 </style>
