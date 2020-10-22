6924acc5b2184e6ca0858c3866c1bbe8d49aefbc
Alvi Akbar <alvi.akbar@outlook.com>
2020-10-21 23:57:55 -0600

    used mutations

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -2,11 +2,12 @@
   <div id="product-list-one">
     <h2>Product List One</h2>
     <ul>
-      <li :key="product.id" v-for="product in products">
+      <li :key="product.id" v-for="product in saleProducts">
         <span class="name">{{ product.name }}</span>
         <span class="price">£{{ product.price }}</span>
       </li>
     </ul>
+    <button @click="reducePrice">Reduce Price</button>
   </div>
 </template>
 
@@ -15,6 +16,14 @@ export default {
   computed: {
     products() {
       return this.$store.state.products;
+    },
+    saleProducts() {
+      return this.$store.getters.saleProducts;
+    },
+  },
+  methods: {
+    reducePrice: function () {
+      this.$store.commit("reducePrice");
     },
   },
 };
